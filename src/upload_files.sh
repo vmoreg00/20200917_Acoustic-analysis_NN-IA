760b8200759b7dcd4390269f865ff2deb0377913
vmoreg00 <vmoreg00@estudiantes.unileon.es>
2021-03-17 11:01:14 +0100

    upload_files.sh modified to better handle files and arguments

--- src/upload_files.sh
+++ src/upload_files.sh
@@ -6,6 +6,7 @@ do
     1) ID1=${OPTARG};;
     2) ID2=${OPTARG};;
     3) CROW=${OPTARG};;
+    *) echo "wrong arguments"; exit 1;;
   esac
 done
 
@@ -15,28 +16,26 @@ ID2=$(grep -n ^$ID2 ../00_CarrionCrow_DB/data/files.csv | cut -f 1 -d ":")
 ID2=$(($ID2 - $ID1 + 1))
 
 # Create the new directory
-if [ ! -d /media/msi/TOSHIBA\ EXT/$CROW ]; then
-    mkdir /media/msi/TOSHIBA\ EXT/$CROW;
+if [ ! -d ./tmp ]; then
+    mkdir ./tmp;
 fi;
 
 # Downsample and rename wav files
 tail -n +$ID1 ../00_CarrionCrow_DB/data/files.csv | head -n $ID2 \
   | sed 's/\ /\\ /g' \
-  | awk -v cr=$CROW ' \
+  | awk ' \
     BEGIN{FS=";"}
     {
-      print "sox "$5".wav /media/msi/TOSHIBA\\ EXT/"cr"/"$1".wav rate 22000"
+      print "sox "$5".wav ./tmp/"$1".wav rate 22000"
     }' \
   | sh;
 
 # Compress them
-zip /media/msi/TOSHIBA\ EXT/$CROW.zip /media/msi/TOSHIBA\ EXT/$CROW/*;
+tar -czf ./tmp/$CROW.tar.gz ./tmp/*wav;
 
 # Upload zip file
-sshpass -f "pswd.txt" scp -P 50055 /media/msi/TOSHIBA\ EXT/$CROW.zip \
-    victorm@vision.unileon.es:~/crow/selector/data/audios/$CROW.zip;
+sshpass -f "pswd.txt" scp -P 50055 ./tmp/$CROW.tar.gz \
+    victorm@vision.unileon.es:~/crow/selector/data/audios/$CROW.tar.gz;
 
 # Remove temporary files (zip and downsampled wavs)
-rm /media/msi/TOSHIBA\ EXT/$CROW.zip
-rm /media/msi/TOSHIBA\ EXT/$CROW/*
-rmdir /media/msi/TOSHIBA\ EXT/$CROW
+rm -r ./tmp
